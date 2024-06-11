5cab1e761ca54444af49a19921f7bf55e40d9c3c
Boris <68553253+bdkma95@users.noreply.github.com>
2024-06-11 18:28:29 +0200

    Update MyNFT.sol
    Simplified solidity function that I would include in the Ethereum smart contract to facilitate this transfer

--- MyNFT.sol
+++ MyNFT.sol
@@ -1,1 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+// MyNFT is an ERC-721 compliant contract
+contract MyNFT is ERC721, Ownable {
+    uint256 public nextTokenId;
+    
+    constructor() ERC721("MyNFT", "MNFT") {}
+
+    // Mint a new NFT
+    function mint(address to) external onlyOwner {
+        uint256 tokenId = nextTokenId;
+        nextTokenId++;
+        _mint(to, tokenId);
+    }
+
+    // Simplified function to transfer an NFT
+    function transferNFT(address from, address to, uint256 tokenId) external {
+        // Ensure the caller is the owner of the token or approved
+        require(_isApprovedOrOwner(_msgSender(), tokenId), "Not approved or owner");
+
+        // Transfer the token from the current owner to the new owner
+        _transfer(from, to, tokenId);
+    }
+}
