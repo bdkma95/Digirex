ccf1809cb1d0a6913cad6c9d04fd302c86f9c13e
Boris <68553253+bdkma95@users.noreply.github.com>
2024-06-11 18:33:30 +0200

    Update MyNFT.sol

--- MyNFT.sol
+++ MyNFT.sol
@@ -1,14 +1,14 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.24;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol"; //The standard implementation of ERC-721 from OpenZeppelin.
+import "@openzeppelin/contracts/access/Ownable.sol"; //A contract module which provides basic access control, where an account (owner) can be granted exclusive access to specific functions.
 
 // MyNFT is an ERC-721 compliant contract
 contract MyNFT is ERC721, Ownable {
-    uint256 public nextTokenId;
+    uint256 public nextTokenId; //A counter to keep track of the next token ID to be minted.
     
-    constructor() ERC721("MyNFT", "MNFT") {}
+    constructor() ERC721("MyNFT", "MNFT") {} //Initializes the ERC-721 token with a name ("MyNFT") and a symbol ("MNFT").
 
     // Mint a new NFT
     function mint(address to) external onlyOwner {
